1d0da1eaf8dd01912d3105372c5003a293bfb174
Yuriy Vins <yuriy.vins83@gmail.com>
2021-11-07 18:26:51 +0600

    My 3 commit

--- src/MainPackage/Main.java
+++ src/MainPackage/Main.java
@@ -3,7 +3,7 @@ package MainPackage;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Happy new Year!");
+        System.out.println("Hello Pre Progect!");
 
     }
 }
